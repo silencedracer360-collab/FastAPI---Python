1a3e2056c64d36ecdba14507eb6e82649c786237
silencedracer360-collab <260802745+silencedracer360-collab@users.noreply.github.com>
2026-06-08 20:40:00 -0500

    Configuración Enrutador clientes y edición de conexion_bd

---
+++ app/enrutador/clientes.py
@@ -0,0 +1,82 @@
+from fastapi import APIRouter
+from app.conexion_bd import listas_clientes
+from app.model.cliente import *
+
+router = APIRouter()
+
+@router.get("/")
+async def listar_cliente():
+
+    if len(listas_clientes) == 0:
+        return {"mensaje": "No hay clientes registrados"}
+
+    return listas_clientes
+
+
+@router.get("/{id}")
+async def listar_cliente_id(id: int):
+
+    for cliente in listas_clientes:
+
+        if cliente.id == id:
+            return cliente
+
+    return {"error": "Cliente no encontrado"}
+
+
+@router.post("/", response_model=Cliente)
+async def crear_cliente(datos_cliente: ClienteCrear):
+
+    cliente_validado = Cliente.model_validate(
+        datos_cliente.model_dump()
+    )
+
+    cliente_validado.id = len(listas_clientes) + 1
+
+    listas_clientes.append(cliente_validado)
+
+    return cliente_validado
+
+
+@router.put("/{id}")
+async def editar_cliente(
+    id: int,
+    datos_cliente: ClienteEditar
+):
+
+    for i, cliente in enumerate(listas_clientes):
+
+        if cliente.id == id:
+
+            cliente_val = Cliente.model_validate(
+                datos_cliente.model_dump()
+            )
+
+            cliente_val.id = id
+
+            listas_clientes[i] = cliente_val
+
+            return {
+                "mensaje": "Cliente actualizado",
+                "cliente": cliente_val
+            }
+
+    return {"error": "Cliente no encontrado"}
+
+
+@router.delete("/{id}")
+async def eliminar_cliente(id: int):
+
+    for i, cliente in enumerate(listas_clientes):
+
+        if cliente.id == id:
+
+            nombre = cliente.nombre
+
+            del listas_clientes[i]
+
+            return {
+                "mensaje": f"Cliente {nombre} eliminado"
+            }
+
+    return {"error": "Cliente no encontrado"}
--- app/main.py
+++ app/main.py
@@ -1,10 +1,9 @@
 from fastapi import FastAPI, HTTPException
+from app.enrutador import clientes
+from app.enrutador.facturas import *
+from app.enrutador.transacciones import *
 
-from app.model.cliente import (
-    Cliente,
-    ClienteCrear,
-    ClienteEditar
-)
+
 
 from app.model.factura import (
     Factura,
@@ -20,95 +19,15 @@ from app.model.transacciones import (
 
 app = FastAPI()
 
-# ===================================
-# LISTAS SIMULANDO BASE DE DATOS
-# ===================================
 
-listar_clientes: list[Cliente] = []
-listas_facturas: list[Factura] = []
-lista_transacciones: list[Transaccion] = []
-
-
+app.include_router(
+    clientes.router,
+    prefix="/clientes"
+)
 # ===================================
 # CRUD CLIENTES
 # ===================================
 
-@app.get("/clientes")
-async def listar_cliente():
-
-    if len(listar_clientes) == 0:
-        return {"mensaje": "No hay clientes registrados"}
-
-    return listar_clientes
-
-
-@app.get("/clientes/{id}")
-async def listar_cliente_id(id: int):
-
-    for cliente in listar_clientes:
-
-        if cliente.id == id:
-            return cliente
-
-    return {"error": "Cliente no encontrado"}
-
-
-@app.post("/clientes", response_model=Cliente)
-async def crear_cliente(datos_cliente: ClienteCrear):
-
-    cliente_validado = Cliente.model_validate(
-        datos_cliente.model_dump()
-    )
-
-    cliente_validado.id = len(listar_clientes) + 1
-
-    listar_clientes.append(cliente_validado)
-
-    return cliente_validado
-
-
-@app.put("/clientes/{id}")
-async def editar_cliente(
-    id: int,
-    datos_cliente: ClienteEditar
-):
-
-    for i, cliente in enumerate(listar_clientes):
-
-        if cliente.id == id:
-
-            cliente_val = Cliente.model_validate(
-                datos_cliente.model_dump()
-            )
-
-            cliente_val.id = id
-
-            listar_clientes[i] = cliente_val
-
-            return {
-                "mensaje": "Cliente actualizado",
-                "cliente": cliente_val
-            }
-
-    return {"error": "Cliente no encontrado"}
-
-
-@app.delete("/clientes/{id}")
-async def eliminar_cliente(id: int):
-
-    for i, cliente in enumerate(listar_clientes):
-
-        if cliente.id == id:
-
-            nombre = cliente.nombre
-
-            del listar_clientes[i]
-
-            return {
-                "mensaje": f"Cliente {nombre} eliminado"
-            }
-
-    return {"error": "Cliente no encontrado"}
 
 
 # ===================================
