3b4c539cb115d4fc1fd6fbda885bd989794439c1
silencedracer360-collab <260802745+silencedracer360-collab@users.noreply.github.com>
2026-06-29 21:59:29 -0500

    Tablas con sus Relaciones

--- app/model/factura.py
+++ app/model/factura.py
@@ -1,23 +1,19 @@
 from pydantic import BaseModel, computed_field
 from datetime import datetime
-
+from sqlmodel import SQLModel, Field, Relationship
 from app.model.cliente import Cliente
 from app.model.transacciones import Transaccion
 
 
-class FacturaBase(BaseModel):
-    fecha: datetime
-    cliente: Cliente
-    transacciones: list[Transaccion] = []
+class FacturaBase(SQLModel):
+    fecha: str = Field(default=datetime.now())
+    # cliente: Cliente
+    # transacciones: list[Transaccion] = []
 
     @computed_field
     @property
     def valor_total(self) -> float:
-        return sum(
-            t.cantidad * t.vr_unitario
-            for t in self.transacciones
-        )
-
+        return 0.0
 
 class CrearFactura(FacturaBase):
     pass
@@ -27,5 +23,6 @@ class EditarFactura(FacturaBase):
     pass
 
 
-class Factura(FacturaBase):
-    id: int | None = None
+class Factura(FacturaBase, table=True   ):
+    id: int | None = Field(default=None, primary_key=True)
+    cliente_id: int = Field(default=None, foreign_key="cliente.id")
--- app/model/transacciones.py
+++ app/model/transacciones.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
+from sqlmodel import SQLModel, Field, Relationship
 
-
-class TransaccionBase(BaseModel):
-    cantidad: int
-    vr_unitario: float
-    descripcion: str
+class TransaccionBase(SQLModel):
+    cantidad: int = Field(default=0)
+    vr_unitario: float = Field(default=0.0)
+    descripcion: str = Field(default=None)
 
 
 class TransaccionCrear(TransaccionBase):
@@ -15,6 +15,6 @@ class TransaccionEditar(TransaccionBase):
     pass
 
 
-class Transaccion(TransaccionBase):
-    id: int | None = None
-    factura_id: int | None = None
+class Transaccion(TransaccionBase, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    factura_id: int | None = Field(default=None, foreign_key="factura.id")
